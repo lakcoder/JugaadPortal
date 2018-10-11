c8f4ee28308963331a242e1aaf72e2cfa23c0afd
Sagar Bansal <sagarbansal099@gmail.com>
2018-10-11 17:44:31 +0000

    init

---
+++ footer.php
@@ -0,0 +1,91 @@
+        <footer class="g-bg-color--dark">
+            <!-- Sponsors -->
+            <div class="footer">
+                <div class="g-text-center--xs g-margin-b-40--xs">
+                    <!--<p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--primary g-letter-spacing--2 g-margin-b-25--xs">Partners</p>-->
+                    <h2 class="g-font-size-24--xs g-font-size-36--md g-color--white">Under patronage of</h2>
+                </div>
+                <div class="container g-padding-y-40--xs g-padding-y-40--sm" id="partners">
+                    <!-- Swiper Clients -->
+                    <div class="container" id="foot">
+                        <div class="row">
+
+                            <div class="col-md-2 col-sm-3 col-xs-6">
+                                <div class="wow fadeIn" data-wow-duration=".3" data-wow-delay=".1s">
+                                    <img class="s-clients-v1" src="img/clients/Make_In_India.png" alt="Clients Logo">
+                                </div>
+                            </div>
+
+
+                            <div class="col-md-2 col-sm-3 col-xs-6">
+                                <div class="wow fadeIn" data-wow-duration=".3" data-wow-delay=".3s">
+                                    <img class="s-clients-v1" src="img/clients/TiE.png" alt="Clients Logo">
+                                </div>
+                            </div>
+                            <div class="col-md-2 col-sm-3 col-xs-6">
+                                <div class="wow fadeIn" data-wow-duration=".3" data-wow-delay=".4s">
+                                    <img class="s-clients-v1" src="img/clients/CIVN.png" alt="Clients Logo">
+                                </div>
+                            </div>
+                            <div class="col-md-2 col-sm-3 col-xs-6">
+                                <div class="wow fadeIn" data-wow-duration=".3" data-wow-delay=".5s">
+                                    <img class="s-clients-v1" src="img/clients/pmyuva.png" alt="Clients Logo">
+                                </div>
+                            </div>
+                            <div class="col-md-2 col-sm-3 col-xs-6">
+                                <div class="wow fadeIn" data-wow-duration=".3" data-wow-delay=".2s">
+                                    <img class="s-clients-v1" src="img/clients/digital-india.png" alt="Clients Logo">
+                                </div>
+                            </div>
+                            <div class="col-md-2 col-sm-3 col-xs-6">
+                                <div class="wow fadeIn" data-wow-duration=".3" data-wow-delay=".5s">
+                                    <img class="s-clients-v1" src="img/clients/vnit_aa.png" alt="Clients Logo">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Swiper Clients -->
+                </div>
+            </div>
+            <!-- End Sponsors -->
+            <!-- Copyright -->
+            <div class="container g-padding-y-20--xs">
+                <div class="row">
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+                        <a href="index.php">
+                            <img class="g-width-100--xs g-height-auto--xs" src="img/logo-ecell.png" alt="Ecell Logo">
+                        </a>
+                        <br><br>
+                        <a class="s-header__action-link" href=" https://m.facebook.com/vnitecell/">
+                            <i class="g-padding-r-5--xs ti-facebook"></i>
+                            <span class="g-display-none--xs g-display-inline-block--sm">Facebook</span>
+                        </a>
+                        <a class="s-header__action-link" href="https://twitter.com/ecell_vnit">
+                            <i class="g-padding-r-5--xs ti-twitter"></i>
+                            <span class="g-display-none--xs g-display-inline-block--sm">Twitter</span>
+                        </a>
+                        <a class="s-header__action-link" href=" https://www.instagram.com/ecellvnit/">
+                            <i class="g-padding-r-5--xs ti-instagram"></i>
+                            <span class="g-display-none--xs g-display-inline-block--sm">Instagram</span>
+                        </a>
+                        <a class="s-header__action-link" href=" https://www.linkedin.com/company/6615520/">
+                            <i class="g-padding-r-5--xs ti-linkedin"></i>
+                            <span class="g-display-none--xs g-display-inline-block--sm">LinkedIn</span>
+                        </a>
+                        <a class="s-header__action-link" href=" https://www.youtube.com/channel/UC0KNOmODhqLcEpcfN9qEsIQ">
+                            <i class="g-padding-r-5--xs ti-youtube"></i>
+                            <span class="g-display-none--xs g-display-inline-block--sm">YouTube</span>
+                        </a>
+                    </div>
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+                            <p class="g-color--white">For Association Opportunities Contact:<br><b>association@ecellvnit.org</b></p>
+                            <p class="g-color--white">For Sponsorship Opportunities Contact:<br><b>sponsorship@ecellvnit.org<b></p>
+                            <p class="g-color--white">For Speaking Opportunities Contact:<br><b>speaker@ecellvnit.org</b></p>
+                    </div>
+                    <div class="col-sm-12 col-md-4 col-lg-4 g-text-right--md">
+                        <p class="g-font-size-14--xs g-margin-b-0--xs g-color--white">© <a class="g-color--primary" href="https://www.ecellvnit.org/" target="_blank">E-cell Vnit</a> All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- End Copyright -->
+        </footer>
---
+++ team.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+    <!-- Begin Head -->
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-125403862-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-125403862-1');
+        </script>
+
+        <!-- Basic -->
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>E-CELL | VNIT</title>
+        <meta name="keywords" content="" />
+        <meta name="description" content="E-Cell VNIT aims to promote and develop entrepreneurship culture amongst the students. Throughout the year, we
+                        conduct various events, workshops and speaker
+                        sessions sessions for inspiring and assisting young
+                        entrepreneurs in their journey in the world of
+                        business">
+        <meta name="author" content="Sagar Bansal">
+
+        <!-- Web Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i|Montserrat:400,700" rel="stylesheet">
+
+        <!-- Vendor Styles -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/animate.css" rel="stylesheet" type="text/css"/>
+        <link href="vendor/themify/themify.css" rel="stylesheet" type="text/css"/>
+        <link href="vendor/scrollbar/scrollbar.min.css" rel="stylesheet" type="text/css"/>
+
+        <!-- Theme Styles -->
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="css/global/global.css" rel="stylesheet" type="text/css"/>
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
+        <link rel="apple-touch-icon" href="img/icon.png">
+    </head>
+    <!-- End Head -->
+
+    <!-- Body -->
+    <body>
+
+        <!--========== HEADER ==========-->
+        <?php include("header.php")?>
+        <!--========== END HEADER ==========-->
+
+        <!--========== PROMO BLOCK ==========-->
+        <div class="g-bg-position--center js__parallax-window" style="background: black 50% 0 no-repeat fixed;">
+            <div class="g-container--md g-text-center--xs g-padding-y-100--xs">
+                <h1 class="g-font-size-40--xs g-font-size-50--sm g-font-size-60--md g-color--white g-letter-spacing--1">Our Team</h1>
+            </div>
+        </div>
+        <!--========== END PROMO BLOCK ==========-->
+
+        <!--========== PAGE CONTENT ==========-->
+        <!-- Speakers -->
+        <div class="container g-padding-y-80--xs g-padding-y-125--sm">
+            <div class="row g-overflow--hidden">
+                <div class="col-xs-6 g-full-width--xs g-margin-b-30--xs g-margin-b-0--lg">
+                    <!-- Speaker -->
+                    <div class="center-block g-box-shadow__dark-lightest-v1 g-width-100-percent--xs g-width-400--lg">
+                        <img class="img-responsive g-width-100-percent--xs core" src="img/400x400/fac1.jpg" alt="Image">
+                        <div class="g-position--overlay g-padding-x-30--xs g-padding-y-30--xs g-margin-t-o-60--xs">
+                            <div class="caption">
+                            <!--<div class="g-bg-color--primary g-padding-x-15--xs g-padding-y-10--xs g-margin-b-20--xs">-->
+                                <h4 class="g-font-size-22--xs g-font-size-26--sm g-color--white g-margin-b-0--xs">Dr. Vijayakumar R. P.</h4>
+                            </div>
+                            <p class="g-font-weight--700">Professor Incharge</p>
+                        </div>
+                    </div>
+                    <!-- End Speaker -->
+                </div>
+                <div class="col-xs-6 g-full-width--xs">
+                    <!-- Speaker -->
+                    <div class="center-block g-box-shadow__dark-lightest-v1 g-width-100-percent--xs g-width-400--lg">
+                        <img class="img-responsive g-width-100-percent--xs core" src="img/400x400/fac2.jpg" alt="Image">
+                        <div class="g-position--overlay g-padding-x-30--xs g-padding-y-30--xs g-margin-t-o-60--xs">
+                            <div class="caption">
+                            <!--<div class="g-bg-color--primary g-padding-x-15--xs g-padding-y-10--xs g-margin-b-20--xs">-->
+                                <h4 class="g-font-size-22--xs g-font-size-26--sm g-color--white g-margin-b-0--xs">Dr. Chayan Das</h4>
+                            </div>
+                            <p class="g-font-weight--700">Professor Incharge</p>
+                        </div>
+                    </div>
+                    <!-- End Speaker -->
+                </div>
+            </div>
+            <br><br>
+
+            <!--<div class="row g-overflow--hidden">-->
+            <!--    <div class="col-xs-12 g-full-width--xs g-margin-b-30--xs g-margin-b-0--lg">-->
+                    <!-- Speaker -->
+            <!--        <div class="center-block g-box-shadow__dark-lightest-v1 g-width-100-percent--xs g-width-400--lg">-->
+            <!--            <img class="img-responsive g-width-100-percent--xs" src="img/400x400/sankalp.jpg" alt="Image">-->
+            <!--            <div class="g-position--overlay g-padding-x-30--xs g-padding-y-30--xs g-margin-t-o-60--xs">-->
+            <!--                <div class="g-bg-color--primary g-padding-x-15--xs g-padding-y-10--xs g-margin-b-20--xs">-->
+            <!--                    <h4 class="g-font-size-22--xs g-font-size-26--sm g-color--white g-margin-b-0--xs">Sankalp Kotewar</h4>-->
+            <!--                </div>-->
+            <!--                <p class="g-font-weight--700">President</p>-->
+            <!--                <p>+91 9623478426<br>sankalpkotewar@ecellvnit.org</p>-->
+            <!--            </div>-->
+            <!--        </div>-->
+                    <!-- End Speaker -->
+            <!--    </div>-->
+            <!--</div>-->
+
+
+            <div class="row g-overflow--hidden">
+                <div class="col-xs-6 g-full-width--xs g-margin-b-30--xs g-margin-b-0--lg">
+                    <!-- Speaker -->
+                    <div class="center-block g-box-shadow__dark-lightest-v1 g-width-100-percent--xs g-width-400--lg">
+                        <img class="img-responsive g-width-100-percent--xs core" src="img/400x400/pratik.jpg" alt="Image">
+                        <div class="g-position--overlay g-padding-x-30--xs g-padding-y-30--xs g-margin-t-o-60--xs">
+                            <div class="caption">
+                            <!--<div class="g-bg-color--primary g-padding-x-15--xs g-padding-y-10--xs g-margin-b-20--xs">-->
+                                <h4 class="g-font-size-22--xs g-font-size-26--sm g-color--white g-margin-b-0--xs">Pratik Zodpey</h4>
+                            </div>
+                            <p class="g-font-weight--700">Events and Planning Head</p>
+                            <p>+91 73788 97208<br>pratik@ecellvnit.org</p>
+                        </div>
+                    </div>
+                    <!-- End Speaker -->
+                </div>
+                <div class="col-xs-6 g-full-width--xs g-margin-b-30--xs g-margin-b-0--lg">
+                    <!-- Speaker -->
+                    <div class="center-block g-box-shadow__dark-lightest-v1 g-width-100-percent--xs g-width-400--lg">
+                        <img class="img-responsive g-width-100-percent--xs core" src="img/400x400/sricharan.jpg" alt="Image">
+                        <div class="g-position--overlay g-padding-x-30--xs g-padding-y-30--xs g-margin-t-o-60--xs">
+                            <div class="caption">
+                            <!--<div class="g-bg-color--primary g-padding-x-15--xs g-padding-y-10--xs g-margin-b-20--xs">-->
+                                <h4 class="g-font-size-22--xs g-font-size-26--sm g-color--white g-margin-b-0--xs">Sri Charan Mahadavi</h4>
+                            </div>
+                            <p class="g-font-weight--700">Events and Planning Head</p>
+                            <p>+91 89194 84722<br>sricharan@ecellvnit.org</p>
+                        </div>
+                    </div>
+                    <!-- End Speaker -->
+                </div>
+
+            </div>
+            <div class="row g-overflow--hidden">
+
+                <div class="col-xs-6 g-full-width--xs g-margin-b-30--xs g-margin-b-0--lg">
+                    <!-- Speaker -->
+                    <div class="center-block g-box-shadow__dark-lightest-v1 g-width-100-percent--xs g-width-400--lg">
+                        <img class="img-responsive g-width-100-percent--xs core" src="img/400x400/ayush.jpg" alt="Image">
+                        <div class="g-position--overlay g-padding-x-30--xs g-padding-y-30--xs g-margin-t-o-60--xs">
+                            <div class="caption">
+                                <h4 class="g-font-size-22--xs g-font-size-26--sm g-color--white g-margin-b-0--xs">Ayush Singh</h4>
+                            </div>
+                            <p class="g-font-weight--700">Treasurer</p>
+                            <p>+91 94235 39984<br>ayushsingh@ecellvnit.org</p>
+                        </div>
+                    </div>
+                    <!-- End Speaker -->
+                </div>
+                <div class="col-xs-6 g-full-width--xs g-margin-b-30--xs g-margin-b-0--lg">
+
+                    <!-- Speaker -->
+                    <div class="center-block g-box-shadow__dark-lightest-v1 g-width-100-percent--xs g-width-400--lg">
+                        <img class="img-responsive g-width-100-percent--xs core" src="img/400x400/sagar.jpg" alt="Image">
+                        <div class="g-position--overlay g-padding-x-30--xs g-padding-y-30--xs g-margin-t-o-60--xs">
+                            <div class="caption">
+                                <h4 class="g-font-size-22--xs g-font-size-26--sm g-color--white g-margin-b-0--xs">Sagar Bansal</h4>
+                            </div>
+                            <p class="g-font-weight--700">Web and Technical Head</p>
+                            <p>+91 77580 11192<br>sagarbansal@ecellvnit.org</p>
+                        </div>
+                    </div>
+                    <!-- End Speaker -->
+                </div>
+            </div>
+            <div class="row g-overflow--hidden">
+                <div class="col-xs-6 g-full-width--xs g-margin-b-30--xs g-margin-b-0--lg">
+                    <!-- Speaker -->
+                    <div class="center-block g-box-shadow__dark-lightest-v1 g-width-100-percent--xs g-width-400--lg">
+                        <img class="img-responsive g-width-100-percent--xs core" src="img/400x400/shubham.jpg" alt="Image">
+                        <div class="g-position--overlay g-padding-x-30--xs g-padding-y-30--xs g-margin-t-o-60--xs">
+                            <div class="caption">
+                            <!--<div class="g-bg-color--primary g-padding-x-15--xs g-padding-y-10--xs g-margin-b-20--xs">-->
+                                <h4 class="g-font-size-22--xs g-font-size-26--sm g-color--white g-margin-b-0--xs">Shubham Jadhav</h4>
+                            </div>
+                            <p class="g-font-weight--700">Corporate Relation Head</p>
+                            <p>+91 80076 57591<br>shubhamjadhav@ecellvnit.org</p>
+                        </div>
+                    </div>
+                    <!-- End Speaker -->
+                </div>
+
+                <div class="col-xs-6 g-full-width--xs g-margin-b-30--xs g-margin-b-0--lg">
+                    <!-- Speaker -->
+                    <div class="center-block g-box-shadow__dark-lightest-v1 g-width-100-percent--xs g-width-400--lg">
+                        <img class="img-responsive g-width-100-percent--xs core" src="img/400x400/ashish.jpg" alt="Image">
+                        <div class="g-position--overlay g-padding-x-30--xs g-padding-y-30--xs g-margin-t-o-60--xs">
+                            <div class="caption">
+                                <h4 class="g-font-size-22--xs g-font-size-26--sm g-color--white g-margin-b-0--xs">Ashish Das</h4>
+                            </div>
+                            <p class="g-font-weight--700">Corporate Relation Head</p>
+                            <p>+91 89184 77717<br>ashish@ecellvnit.org</p>
+                        </div>
+                    </div>
+                    <!-- End Speaker -->
+                </div>
+
+
+            </div>
+
+            <div class="row g-overflow--hidden">
+
+                <div class="col-xs-6 g-full-width--xs g-margin-b-30--xs g-margin-b-0--lg">
+                    <!-- Speaker -->
+                    <div class="center-block g-box-shadow__dark-lightest-v1 g-width-100-percent--xs g-width-400--lg">
+                        <img class="img-responsive g-width-100-percent--xs core" src="img/400x400/prashant.jpg" alt="Image">
+                        <div class="g-position--overlay g-padding-x-30--xs g-padding-y-30--xs g-margin-t-o-60--xs">
+                            <div class="caption">
+                            <!--<div class="g-bg-color--primary g-padding-x-15--xs g-padding-y-10--xs g-margin-b-20--xs">-->
+                                <h4 class="g-font-size-22--xs g-font-size-26--sm g-color--white g-margin-b-0--xs">Prashant Bramhanwade</h4>
+                            </div>
+                            <p class="g-font-weight--700">Public Relation Head</p>
+                            <p>+91 70578 56961<br>prashant@ecellvnit.org</p>
+                        </div>
+                    </div>
+                    <!-- End Speaker -->
+                </div>
+                <div class="col-xs-6 g-full-width--xs g-margin-b-30--xs g-margin-b-0--lg">
+                    <!-- Speaker -->
+                    <div class="center-block g-box-shadow__dark-lightest-v1 g-width-100-percent--xs g-width-400--lg">
+                        <img class="img-responsive g-width-100-percent--xs core" src="img/400x400/nachiket.jpg" alt="Image">
+                        <div class="g-position--overlay g-padding-x-30--xs g-padding-y-30--xs g-margin-t-o-60--xs">
+                            <div class="caption">
+                            <!--<div class="g-bg-color--primary g-padding-x-15--xs g-padding-y-10--xs g-margin-b-20--xs">-->
+                                <h4 class="g-font-size-22--xs g-font-size-26--sm g-color--white g-margin-b-0--xs">Nachiket Dhande</h4>
+                            </div>
+                            <p class="g-font-weight--700">Public Relation Head</p>
+                            <p>+91 95453 02811<br>nachiketdhande@ecellvnit.org</p>
+                        </div>
+                    </div>
+                    <!-- End Speaker -->
+                </div>
+
+            </div>
+
+            <!--<div class="row g-overflow--hidden">-->
+
+            <!--</div>-->
+
+
+
+
+
+
+        </div>
+        <!-- End Speakers -->
+
+                <!--========== END PAGE CONTENT ==========-->
+
+        <!--========== FOOTER ==========-->
+        <?php include("footer.php");?>
+        <!--========== END FOOTER ==========-->
+
+        <!-- Back To Top -->
+        <a href="javascript:void(0);" class="s-back-to-top js__back-to-top"></a>
+
+        <!--========== JAVASCRIPTS (Load javascripts at bottom, this will reduce page load time) ==========-->
+        <!-- Vendor -->
+        <script type="text/javascript" src="vendor/jquery.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.migrate.min.js"></script>
+        <script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.smooth-scroll.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.back-to-top.min.js"></script>
+        <script type="text/javascript" src="vendor/scrollbar/jquery.scrollbar.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.parallax.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.wow.min.js"></script>
+
+        <!-- General Components and Settings -->
+        <script type="text/javascript" src="js/global.min.js"></script>
+        <script type="text/javascript" src="js/components/header-sticky.min.js"></script>
+        <script type="text/javascript" src="js/components/scrollbar.min.js"></script>
+        <script type="text/javascript" src="js/components/wow.min.js"></script>
+        <!--========== END JAVASCRIPTS ==========-->
+
+    </body>
+    <!-- End Body -->
+</html>
