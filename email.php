cb3a9e723ebe4111cfa08572c6fbfcecb4ecd438
Sagar Bansal <sagarbansal099@gmail.com>
2018-10-12 19:03:10 +0000

    email

---
+++ email.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            li{
+                padding:10px;
+            }
+            p{
+                font-size:16px;
+            }
+        </style>
+    </head>
+    <body style="width:100%; background-color:#fff; padding:50px 30px; color:1e1e1e; margin-top:100px">
+        <div style="width:90%; background-color:#f7f9fb; padding:50px 30px;">
+            <h3><b>Hello $name,</b></h3>
+            <p style="font-size:18px;">Thank You for registering with us. Your Unique ID is <b>$UNIQUE</b><br>Your Unique Link is <a href='http://jugaad.ecellvnit.org/$UNIQUE.php'>here!</a></p>
+
+        </div>
+        <div style="padding:0px 20px; width:90%; margin-top:100px">
+            <h4>Team Guidelines for Jugaad 18'</h4>
+            <ol>
+            <li>Registration will begin at 9 AM.</li>
+            <li>A team registering at the desk will randomly be assigned a mentor. The team will fill an online form. The team leader will receive an automated email with a link. They will share this link with their mentor.</li>
+            <li>The link opens up a transaction database. The mentor will be able to track each transaction simply by refreshing the page.</li>
+            <li>The team leader will also form a WhatsApp group for the team and the mentor in order to facilitate swift communication and sharing of data.</li>
+            <li>The mentor shall demand regular updates from their assigned teams in the form of photographic, videographic or written evidence of each transaction or of the service being sold.</li>
+            <li>All participating teams must finish their sales by 4 pm (recommended) so that they can report back to the registration desk as clarified below.</li>
+            <li>All participating teams must report to the registration area by 4:30 pm. Failing to do so can result in immediate disqualification of the concerned team.</li>
+            <li>In the registration area, a team will approach their mentor and hand over the Rs. 100 component provided to them initially. The team will also give their profits to the mentor for verification.</li>
+            <li>The team will then be returned their team leader's ID proof. They will also receive participation certificates for each team member.</li>
+            <li>After final evaluation, teams with the top three highest profits will receive first, second and third position respectively.</li>
+            <li>In order to ensure fair play, a part of the profits of the winning teams will be deducted based on the amount. With a bit of forethought, the underlying reasons for this are clear.</li>
+            <li>The profits of all other teams will be returned in whole. The initial investment component of the team will not be considered for final evaluation. It will be deducted from the total amount to arrive at the profits made.</li>
+            <li>There is a special award known as 'Most Innovative Award' to recognize creative and out of the box ideas by teams. This award can go to anyone, even if the profits made are comparatively low.</li>
+            <li>The team winning FIRST position will have an internship opportunity with Ola Cabs.</li>
+            <li>Selection: Each member of the winning team will sit for a personal interview. One person will be finalized for the internship role.</li>
+            <li>The winning teams will receive goodies.</li>
+            <li>All participants are welcome to take pictures at the photo booth in the registration area.</li>
+            <li>All members can approach the organizers for doubts and/or assistance.</li>
+            </ol>
+        </div>
+        <hr>
+        <div style="margin:80px 30px; color:#000">
+            <h5>Contact Us</h5>
+            <p>Lakshya Shukla</p>
+            <p>Event Head</p>
+            <p>+91 77384 46941</p>
+            <p>or</p>
+            <p>Mail Us: contact@ecellvnit.org</p>
+
+        </div>
+
+    </body>
+</html>
+
--- submit.php
+++ submit.php
@@ -52,34 +52,70 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
         $from = "E-CELL VNIT <noreply@ecellvnit.org>";    //your mail id
         $to = $email;
         $subject = "Registration successful";
-        $body = "Thank You $name for registering with us.\n\nYour Unique ID is $UNIQUE\n\nYour Unique Link is jugaad.ecellvnit.org/$teamsize/$UNIQUE.php\n\n
-                    Team Guidelines for Jugaad 17'\n\n
+        $body = '
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <style>
+                    li{
+                        padding:10px;
+                    }
+                    p{
+                        font-size:16px;
+                    }
+                </style>
+            </head>
+            <body style="width:100%; background-color:#fff; padding:50px 30px; color:1e1e1e; margin-top:100px">
+                <div style="width:90%; background-color:#f7f9fb; padding:50px 30px;">
+                    <h3><b>Hello $name,</b></h3>
+                    <p style="font-size:18px;">Thank You for registering with us. Your Unique ID is <b>$UNIQUE</b><br>Your Unique Link is <a href="http://jugaad.ecellvnit.org/$UNIQUE.php">here!</a></p>
 
-    A participating team will comprise 3-5 members.\n
-    Registration will begin at 9 AM.\n
-    A team registering at the desk will randomly be assigned a mentor. The team will fill an offline form as well as an online form. The team leader will receive an automated email with a link. They will share this link with their mentor.\n
-    The link opens up a transaction database. The mentor will be able to track each transaction simply by refreshing the page.\n
-    The team leader will also form a WhatsApp group for the team and the mentor in order to facilitate swift communication and sharing of data.\n
-    The mentor shall demand regular updates from their assigned teams in the form of photographic, videographic or written evidence of each transaction or of the service being sold.\n
-    All participating teams must finish their sales by 4 pm (recommended) so that they can report back to the registration desk as clarified below.\n
-    All participating teams must report to the registration area by 4:30 pm. Failing to do so can result in immediate disqualification of the concerned team.\n
-    In the registration area, a team will approach their mentor and hand over the Rs. 100 component provided to them initially. The team will also give their profits to the mentor for verification.\n
-    The team will then be returned their team leader's ID proof. They will also receive participation certificates for each team member.\n
-    After final evaluation, teams with the top three highest profits will receive first, second and third position respectively.\n
-    In order to ensure fair play, a part of the profits of the winning teams will be deducted based on the amount. With a bit of forethought, the underlying reasons for this are clear.\n
-    The profits of all other teams will be returned in whole.\n
-    The initial investment component of the team will not be considered for final evaluation. It will be deducted from the total amount to arrive at the profits made.\n
-    There is a special award known as 'Most Innovative Award' to recognize creative and out of the box ideas by teams. This award can go to anyone, even if the profits made are comparatively low.\n
-    The team winning FIRST position will have an internship opportunity with Ola Cabs. Selection: Each member of the winning team will sit for a personal interview. One person will be finalized for the internship role.\n
-    The winning teams will receive goodies.\n
-    All participants are welcome to take pictures at the photo booth in the registration area.\n
-    All members can approach the organizers for doubts and/or assistance.";
+                </div>
+                <div style="padding:0px 20px; width:90%; margin-top:100px">
+                    <h4>Team Guidelines for Jugaad 18\'</h4>
+                    <ol>
+                    <li>Registration will begin at 9 AM.</li>
+                    <li>A team registering at the desk will randomly be assigned a mentor. The team will fill an online form. The team leader will receive an automated email with a link. They will share this link with their mentor.</li>
+                    <li>The link opens up a transaction database. The mentor will be able to track each transaction simply by refreshing the page.</li>
+                    <li>The team leader will also form a WhatsApp group for the team and the mentor in order to facilitate swift communication and sharing of data.</li>
+                    <li>The mentor shall demand regular updates from their assigned teams in the form of photographic, videographic or written evidence of each transaction or of the service being sold.</li>
+                    <li>All participating teams must finish their sales by 4 pm (recommended) so that they can report back to the registration desk as clarified below.</li>
+                    <li>All participating teams must report to the registration area by 4:30 pm. Failing to do so can result in immediate disqualification of the concerned team.</li>
+                    <li>In the registration area, a team will approach their mentor and hand over the Rs. 100 component provided to them initially. The team will also give their profits to the mentor for verification.</li>
+                    <li>The team will then be returned their team leader\'s ID proof. They will also receive participation certificates for each team member.</li>
+                    <li>After final evaluation, teams with the top three highest profits will receive first, second and third position respectively.</li>
+                    <li>In order to ensure fair play, a part of the profits of the winning teams will be deducted based on the amount. With a bit of forethought, the underlying reasons for this are clear.</li>
+                    <li>The profits of all other teams will be returned in whole. The initial investment component of the team will not be considered for final evaluation. It will be deducted from the total amount to arrive at the profits made.</li>
+                    <li>There is a special award known as \'Most Innovative Award\' to recognize creative and out of the box ideas by teams. This award can go to anyone, even if the profits made are comparatively low.</li>
+                    <li>The team winning FIRST position will have an internship opportunity with Ola Cabs.</li>
+                    <li>Selection: Each member of the winning team will sit for a personal interview. One person will be finalized for the internship role.</li>
+                    <li>The winning teams will receive goodies.</li>
+                    <li>All participants are welcome to take pictures at the photo booth in the registration area.</li>
+                    <li>All members can approach the organizers for doubts and/or assistance.</li>
+                    </ol>
+                </div>
+                <hr>
+                <div style="margin:80px 30px; color:#000">
+                    <h5>Contact Us</h5>
+                    <p>Lakshya Shukla<br>
+                    (Event Head)</p>
+                    <p>+91 77384 46941</p>
+                    <p>or</p>
+                    <p>Mail Us: contact@ecellvnit.org</p>
+
+                </div>
+
+            </body>
+        </html>';
+
         $host = "ssl://sharedlinux.cloudhostdns.net";
         $port = "465";
         $username = "noreply@ecellvnit.org";          //your mail id
         $password = "VNIT@123";                      //password of this mail id
 
-        $headers = array('From' => $from,
+        $headers = array('MIME-Version' => '1.0rn',
+            'Content-Type' => "text/html; charset=ISO-8859-1rn",
+            'From' => $from,
             'To' => $to,
             'Subject' => $subject);
         $smtp = Mail::factory('smtp',
@@ -92,7 +128,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
         $mail = $smtp->send($to, $headers, $body);
 
     }
-    $myFile = "$UNIQUE+$SIZE.php"; // or .php
+    $myFile = "$UNIQUE.php"; // or .php
     $fh = fopen($myFile, 'w'); // or die("error");
     $stringData = file_get_contents("transactions.php");
     fwrite($fh, $stringData);
